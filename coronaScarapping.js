c2582f970aff0bd9feb1c4194394b694bbc7ef52
Dhruv-gupta1001 <56994218+Dhruv-gupta1001@users.noreply.github.com>
2022-06-29 17:44:04 +0530

    Add files via upload

---
+++ coronaScarapping.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const cheerio = require("cheerio");
+const chalk=require('chalk');
+
+console.log(chalk.blue("hello world"));
+console.log("Before");
+request('https://www.worldometers.info/coronavirus', cb);
+
+console.log("After")
+function cb(error, response, html) {
+    if (error) {
+        console.error('error:', error);
+    } else {
+        handlehtml(html);
+
+    }
+}
+function handlehtml(html) {
+    let selTool = cheerio.load(html);
+    let contentArr = selTool("#maincounter-wrap span");
+    
+    let total = selTool(contentArr[0]).text();
+    let deaths = selTool(contentArr[1]).text();
+    let recovered = selTool(contentArr[2]).text();
+
+    console.log(chalk.gray("Total Cases: "+total));
+    console.log(chalk.red("Deaths: "+deaths));
+    console.log(chalk.green("Recovery : "+recovered));
+
+
+}
